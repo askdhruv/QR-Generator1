15d94e58bb3328f3d4fc5700dec3f847bb99a741
Dhruv Aggarwal <dhruv.2002agg@gmail.com>
2023-01-12 16:12:19 +0530

    First project using node

---
+++ index.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */{
+        name: "link",
+        message: "What is the link",
+        type: 'input'        
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.link;
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream("qr-image.png"));
+
+    fs.writeFileSync("link.txt", url, (err) => {
+    if(err)
+        console.log(err);
+    else    
+        console.log("Success");
+});
+
+})
+
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+//Add React frontend
