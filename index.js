b7ba469144278e5d9748a8a31dc29ff50cf163c1
Dhruv Aggarwal <dhruv.2002agg@gmail.com>
2023-02-24 16:31:35 +0530

    Connecting frontend with backend

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const qr = require('qr-image');
 const fs = require('fs');
 
+
 const port = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
